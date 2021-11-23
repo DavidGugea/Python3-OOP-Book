0ed6d3e29e4b026b57974eb4ef377f7a299cccbb
David Ioannis Gugea <davidgugea@yahoo.com>
2021-11-24 00:08:15 +0100

    Set and Dictionary comprehensions

---
+++ Chapter7/Comprehensions/SetAndDictionaryComprehensions/set_comprehension.py
@@ -0,0 +1,15 @@
+from collections import namedtuple
+
+Book = namedtuple("Book", "author title genre")
+books = [
+    Book("Author 1", "Title 1", "Genre 1"),
+    Book("Author 2", "Title 2", "Genre 1"),
+    Book("Author 3", "Title 3", "Genre 2"),
+    Book("Author 4", "Title 4", "Genre 2"),
+    Book("Author 5", "Title 5", "Genre 3"),
+    Book("Author 6", "Title 6", "Genre 3"),
+    Book("Author 7", "Title 7", "Genre 4")
+]
+
+genre_3_authors = {b.author for b in books if b.genre == "Genre 3"}
+print(genre_3_authors)
